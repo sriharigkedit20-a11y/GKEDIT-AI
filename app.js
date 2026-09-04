2c62b3c701bf2cdc74a6074b8faa8b6303c2eb26
sriharigkedit20-a11y <sriharigkedit20@gmail.com>
2026-09-04 06:28:42 +0530

    Add handleClick function for button click event

---
+++ app.js
@@ -0,0 +1,5 @@
+function handleClick() {
+  const text = document.getElementById('status-text');
+  text.innerText = "Button clicked! App logic working smoothly.";
+  text.style.color = "#4ade80";
+}
